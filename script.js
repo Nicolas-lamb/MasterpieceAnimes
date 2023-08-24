62f14eed457adaf40d5ac61b68d4f1bb0707f6c3
nicolas-lamb <nikelamb@gmail.com>
2023-08-24 10:16:13 -0300

    q

--- script.js
+++ script.js
@@ -6,8 +6,36 @@ function clickMenu(){
         teste.style.display = 'block'
     }
 }
-function trailer(){
-    if(temporada.style.display == 'flex' ){
-        iframe.style.display =='flex'
+// traller
+function clickImage1(){
+    if(testeframe1.style.display == 'block'){
+        testeframe1.style.display = 'none'
+    }
+    else{
+        testeframe1.style.display = 'block'
+    }
+}
+function clickImage2(){
+    if(testeframe2.style.display == 'block'){
+        testeframe2.style.display = 'none'
+    }
+    else{
+        testeframe2.style.display = 'block'
+    }
+}
+function clickImage3(){
+    if(testeframe3.style.display == 'block'){
+        testeframe3.style.display = 'none'
+    }
+    else{
+        testeframe3.style.display = 'block'
+    }
+}
+function clickImage4(){
+    if(testeframe4.style.display == 'block'){
+        testeframe4.style.display = 'none'
+    }
+    else{
+        testeframe4.style.display = 'block'
     }
 }
