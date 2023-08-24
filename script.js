63e6b5c3bd1c7b139ab529f2eb7e44de705914de
nicolas-lamb <nikelamb@gmail.com>
2023-08-24 11:35:41 -0300

    qa

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// JavaScript para alternar o menu de navegação móvel
+
+
+
+// menu responsivo
 function clickMenu(){
     if(teste.style.display == 'block'){
         teste.style.display = 'none'
@@ -6,6 +11,52 @@ function clickMenu(){
         teste.style.display = 'block'
     }
 }
+// carroceu
+const carousel = document.querySelector(".carousel");
+const melhores = document.querySelector(".melhores");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+const prevBtn2 = document.getElementById("prevBtn2");
+const nextBtn2 = document.getElementById("nextBtn2");
+
+const images = Array.from(carousel.getElementsByTagName("img"));
+const images2 = Array.from(melhores.getElementsByTagName("img"));
+const numVisible = 4; // Quantidade de imagens visíveis ao mesmo tempo
+let currentIndex = 0;
+
+nextBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex + numVisible) % images.length;
+    updateCarousel();
+});
+
+prevBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex - numVisible + images.length) % images.length;
+    updateCarousel();
+});
+
+nextBtn2.addEventListener("click", () => {
+    currentIndex = (currentIndex + numVisible) % images2.length;
+    updateMelhores();
+});
+
+prevBtn2.addEventListener("click", () => {
+    currentIndex = (currentIndex - numVisible + images2.length) % images2.length;
+    updateMelhores();
+});
+
+function updateCarousel() {
+    const offset = -currentIndex * (100 / numVisible);
+    carousel.style.transform = `translateX(${offset}%)`;
+}
+
+function updateMelhores() {
+    const offset = -currentIndex * (100 / numVisible);
+    melhores.style.transform = `translateX(${offset}%)`;
+}
+// Chame a função inicial para garantir que as imagens iniciais estejam visíveis
+
+// Configuração para o carrossel com ID "temporada"
+setupCarousel("temporada");
 // traller
 function clickImage1(){
     if(testeframe1.style.display == 'block'){
@@ -39,3 +90,28 @@ function clickImage4(){
         testeframe4.style.display = 'block'
     }
 }
+function clickImage5(){
+        if(testeframe5.style.display == 'block'){
+            testeframe5.style.display = 'none'
+        }
+        else{
+            testeframe5.style.display = 'block'
+        }
+}
+function clickImage6(){
+    if(testeframe6.style.display == 'block'){
+        testeframe6.style.display = 'none'
+    }
+    else{
+        testeframe6.style.display = 'block'
+    }
+}
+function clickImage5(){
+    if(testeframe7.style.display == 'block'){
+        testeframe7.style.display = 'none'
+    }
+    else{
+        testeframe7.style.display = 'block'
+    }
+}
+
